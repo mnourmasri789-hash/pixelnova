01334c0a24f42076e33e55ee62d93103a93f5567
mnourmasri789-hash <m.nourmasri789@gmail.com>
2026-06-25 14:42:10 +0300

    Update test_db.php

--- test_db.php
+++ test_db.php
@@ -1,23 +1,8 @@
 <?php
 
-echo "PDO: ";
+echo "PHP Works<br>";
+
+var_dump(extension_loaded('pdo'));
+echo "<br>";
+
 var_dump(extension_loaded('pdo_mysql'));
-
-echo "<br>MYSQLI: ";
-var_dump(extension_loaded('mysqli'));
-
-echo "<br><br>";
-
-try {
-    $pdo = new PDO(
-        "mysql:host=" . getenv('MYSQLHOST') .
-        ";port=" . getenv('MYSQLPORT') .
-        ";dbname=" . getenv('MYSQLDATABASE'),
-        getenv('MYSQLUSER'),
-        getenv('MYSQLPASSWORD')
-    );
-
-    echo "CONNECTED SUCCESSFULLY";
-} catch (Exception $e) {
-    echo $e->getMessage();
-}
