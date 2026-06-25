475f9a1dbeb7965f7a5811330c45128dfd1581f5
mnourmasri789-hash <m.nourmasri789@gmail.com>
2026-06-25 16:20:57 +0300

    Update test.php

--- test.php
+++ test.php
@@ -1,3 +1,10 @@
 <?php
-echo "HELLO";
-?>
+
+echo "PDO: ";
+var_dump(extension_loaded('pdo'));
+
+echo "<br>PDO_MYSQL: ";
+var_dump(extension_loaded('pdo_mysql'));
+
+echo "<br>MYSQLI: ";
+var_dump(extension_loaded('mysqli'));
