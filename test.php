f751a1f0bdb87ae589dbb8cbea99700386b322db
mnourmasri789-hash <m.nourmasri789@gmail.com>
2026-06-25 14:23:40 +0300

    Delete test.php

--- test.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-echo "HOST: " . getenv('MYSQLHOST') . "<br>";
-echo "DB: " . getenv('MYSQLDATABASE') . "<br>";
-echo "USER: " . getenv('MYSQLUSER') . "<br>";
-echo "PASS: " . (getenv('MYSQLPASSWORD') ? 'FOUND' : 'NOT FOUND') . "<br>";
-echo "PORT: " . getenv('MYSQLPORT') . "<br>";
-
-?>
